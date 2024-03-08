058a7ee13992d1a3c6d4e4998b1cbf05dd63b254
NicolasPonciano <niickponciano@gmail.com>
2024-03-08 13:31:35 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿float bas, alt, res;
+
+Console.WriteLine("Digite Base:");
+bas = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a altura");
+alt = float.Parse(Console.ReadLine());
+
+res = bas * alt / 2;
+
+Console.WriteLine("A area é " + res);
+
+Console.ReadKey();
